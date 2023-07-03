357ce61d0678331125a84b057e78477b0cca44b3
Valery Logvinov <valera.logvinov.97@mail.ru>
2023-07-03 23:50:26 +0300

    add file README

---
+++ README.md
@@ -0,0 +1,5 @@
+FormCalculate
+
+Ссылка на deploy проекта: http://cz75154.tw1.ru/
+
+Для запуска на локальном пк создайте файл config.php (пример example.config.php)
---
+++ backend/example.config.php
@@ -0,0 +1,8 @@
+<?php
+
+const PORT = PORT; // порт базы данных
+const HOST = 'localhost'; // хостинг где лежит база данных
+const DBBAME = 'DBBAME'; // название базы данных
+const USER = 'USER'; // имя пользователя базы данных 
+const PASS = 'PASS'; // пароль базы данных
+const DEBUG = true; // если запускаем на сервере отключает нужно, чтобы выводились ошибки
